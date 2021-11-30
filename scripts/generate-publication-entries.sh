e101bc912053c42d608e16f5de5ae490fb7318b6
Gabor Szarnyas <szarnyasg@gmail.com>
2021-11-30 12:11:16 +0100

    Bump version of academic package, fix output directory location

--- scripts/generate-publication-entries.sh
+++ scripts/generate-publication-entries.sh
@@ -6,4 +6,10 @@ set -o pipefail
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ..
 
-find ./bibs/ -iname "*bib" -exec ~/.local/bin/academic import --overwrite --publication-dir=english/publication --verbose --bibtex {} \;
+find ./bibs/ -iname "*bib" \
+    -exec ~/.local/bin/academic import \
+        --overwrite \
+        --publication-dir=content/english/publication \
+        --verbose \
+        --bibtex {} \
+    \;
--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -6,4 +6,4 @@ set -o pipefail
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ..
 
-pip3 install -U academic==0.7.0
+pip3 install -U academic==0.8.1
