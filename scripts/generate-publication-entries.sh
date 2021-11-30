3ace6ab62defc6efea9c94f0e68cc563ee1c7f7b
Gabor Szarnyas <szarnyasg@gmail.com>
2021-11-30 12:45:22 +0100

    Use subtitle field for Internet Archive URL

--- scripts/generate-publication-entries.sh
+++ scripts/generate-publication-entries.sh
@@ -6,6 +6,8 @@ set -o pipefail
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ..
 
+# hack: use the 'subtitle' field to pass the Internet Archive URL through the academic CLI script
+sed -i.bkp 's/ia_url/subtitle/' ./bibs/*.bib
 find ./bibs/ -iname "*bib" \
     -exec ~/.local/bin/academic import \
         --overwrite \
@@ -13,3 +15,4 @@ find ./bibs/ -iname "*bib" \
         --verbose \
         --bibtex {} \
     \;
+rm ./bibs/*.bib.bkp
